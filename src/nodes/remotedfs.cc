ed6882742a8f6a5c45cb480f4d951334e61cd94a
Wonbae Kim <zwigul@gmail.com>
2016-03-17 00:28:03 +0900

    separate idata part from remotedfs

--- src/nodes/remotedfs.cc
+++ src/nodes/remotedfs.cc
@@ -136,21 +136,3 @@ void RemoteDFS::send_block (std::string k, std::string v) {
   network->send(0, &bi);
 }
 // }}}
-
-
-
-void RemoteDFS::insert_idata(messages::Message* msg) {
-  auto idata_info = dynamic_cast<messages::IDataInfo*>(msg);
-  logger->info("IDataInfo received.");
-  peer.insert_idata(idata_info);
-}
-void RemoteDFS::insert_igroup(messages::Message* msg) {
-  auto igroup_info = dynamic_cast<messages::IGroupInfo*>(msg);
-  logger->info("IGroupInfo received.");
-  peer.insert_igroup(igroup_info);
-}
-void RemoteDFS::insert_iblock(messages::Message* msg) {
-  auto iblock_info = dynamic_cast<messages::IBlockInfo*>(msg);
-  logger->info("IBlockInfo received.");
-  peer.insert_iblock(iblock_info);
-}
