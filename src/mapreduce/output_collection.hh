2b6f6eb223c64e3ef19c5ccf90a3379640f5cd22
deukyeon <deukyeon.hwang@gmail.com>
2017-01-04 13:18:37 +0900

    added missing files

---
+++ src/mapreduce/output_collection.hh
@@ -0,0 +1,33 @@
+#pragma once
+
+#include <map>
+#include <vector>
+#include <string>
+#include <functional>
+
+namespace velox {
+  class OutputCollection {
+    public:
+      OutputCollection();
+      ~OutputCollection();
+
+      /* TODO: arguments for any types */
+      bool insert(std::string, std::string);
+
+      auto begin();
+      auto end();
+
+      /* TODO: arguments for any types */
+      void travel(std::function<void(std::string, std::vector<std::string>*)>);
+
+      void print_all();
+
+    private:
+      using key_t = std::string;
+      using value_t = std::vector<std::string>*;
+
+      std::map<key_t, value_t>* collection_; 
+
+      void check_or_alloc_collection();
+  };
+}
